d05c59d98e2128797d5fb6e425d905c97bbe799f
Nicholas Levenson <nicklevenson@gmail.com>
2021-04-05 18:12:46 -0700

    tests

---
+++ spec/models/notification_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Notification, type: :model do
+  describe "associations" do 
+    it {should belong_to(:user).class_name("User")}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'create user' do
+  describe 'validations' do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:location) }
+  end
+  describe 'associations' do
+    it { should have_many(:connections).class_name('Connection') }
   end
 end
