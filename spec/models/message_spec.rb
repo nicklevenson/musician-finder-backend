c59aa80b21c60aecf2e09b4ca7c85e6908065239
Nicholas Levenson <nicklevenson@gmail.com>
2021-04-12 15:10:41 -0700

    initial chat tests

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:user).class_name('User') }
+    it { should belong_to(:chatroom).class_name('Chatroom') }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:content)}
+  end
 end
--- spec/models/userchatroom_spec.rb
+++ spec/models/userchatroom_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Userchatroom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe Userchatroom, type: :model do
+#   describe "associations" do
+
+#   end
+
+  
+# end
