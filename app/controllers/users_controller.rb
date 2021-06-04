0e2be959da5b9be9692afbef4bc3e1d89717d6cb
Nicholas Levenson <nicklevenson@gmail.com>
2021-06-04 11:20:11 -0700

    allows lat and lng into params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -129,7 +129,11 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:bio, :location, tags_attributes: [:name, :tag_type, :spotify_uri, :spotify_image_url, :spotify_link], genres_attributes: [:name], instruments_attributes: [:name])
+      params.require(:user).permit(
+        :bio, :location, :lat, :lng, 
+        tags_attributes: [:name, :tag_type, :spotify_uri, :spotify_image_url, :spotify_link], 
+        genres_attributes: [:name], instruments_attributes: [:name]
+      )
     end
 
     def recommended_users_params
